e4b29a27d92ecfb2c60bf535d572dd2e1083536a
ramanverma2k <ramanverma4183@gmail.com>
2021-07-21 00:51:34 +0530

    fetch weatherData from openweather api and city name using coordinates

---
+++ lib/services/openweather.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart';
+import 'package:weatherium/services/location.dart';
+
+Future<Map<String, dynamic>?> getWeatherData() async {
+  Location location = new Location();
+  Position position = await location.determinePosition();
+  String? cityName = await location.getCityNameFromCoordinates(
+      position.latitude, position.longitude);
+
+  String _apiKey = '';
+  String _url =
+      'https://api.openweathermap.org/data/2.5/onecall?lat=${position.latitude}&lon=${position.longitude}&units=metric&exclude=minutely&appid=$_apiKey';
+
+  Response response = await get(Uri.parse(_url));
+  if (response.statusCode == 200) {
+    Map<String, dynamic> weatherData = jsonDecode(response.body);
+    weatherData['city'] = cityName;
+    return weatherData;
+  } else {
+    debugPrint('${response.statusCode}');
+  }
+}
